f66de1a78599af8894b603f687b9aa140d678cfa
Simon Uhrmann <simon.uhrmann@gmail.com>
2017-10-14 05:05:29 +0200

    Refactoring

--- app/src/main/java/de/mordsgau/phase2/card/CardFactory.java
+++ app/src/main/java/de/mordsgau/phase2/card/CardFactory.java
@@ -21,20 +21,12 @@ import de.mordsgau.phase2.adapter.RecyclerAdapter;
 
 public class CardFactory {
 
-    private static final String[] sampleLabels = {"a", "b", "c", "d", "e", "f", "g", "h", "i"};
-
-    private static final float[][] sampleValues = {{0f, 2f, 1.4f, 4.f, 3.5f, 4.3f, 2f, 4f, 6.f},
-            {1.5f, 2.5f, 1.5f, 5f, 4f, 5f, 4.3f, 2.1f, 1.4f}};
-
-    private static final String[] sampleLabels_batteryState =
-            {"Auto", "Haus"};
-
-    private static final float[][] sampleValues_batteryState =
-            {{50f, 20f},  // % filled
-            { 20f, 75f},  // % public used
-            { 30f,  5f}}; // % not charged / empty
-
     public static void buildPowerConsumption(RecyclerAdapter.ViewHolder card) {
+        // Sample values
+        final String[] sampleLabels = {"2:00", "4:00", "6:00", "8:00", "10:00", "12:00", "14:00", "16:00", "18:00"};
+        final float[][] sampleValues = {{0f, 2f, 1.4f, 4.f, 3.5f, 4.3f, 2f, 4f, 6.f},
+                {1.5f, 2.5f, 1.5f, 5f, 4f, 5f, 4.3f, 2.1f, 1.4f}};
+
 
         card.textView.setText(R.string.card_power_consumption);
 
@@ -45,20 +37,25 @@ public class CardFactory {
         LineSet dataset = new LineSet(sampleLabels, sampleValues[0]);
         dataset.setColor(Color.parseColor("#53c1bd"))
                 .setFill(Color.parseColor("#3d6c73"))
-                .setGradientFill(new int[] {Color.parseColor("#364d5a"), Color.parseColor("#3f7178")},
+                .setGradientFill(new int[]{Color.parseColor("#364d5a"), Color.parseColor("#3f7178")},
                         null);
         chart.addData(dataset);
 
         chart.setBorderSpacing(1)
-                .setXLabels(AxisRenderer.LabelPosition.NONE)
-                .setYLabels(AxisRenderer.LabelPosition.NONE);
-        chart.setXAxis(true);
-        chart.setYAxis(false);
+                .setXLabels(AxisRenderer.LabelPosition.OUTSIDE)
+                .setYLabels(AxisRenderer.LabelPosition.OUTSIDE);
 
         chart.show();
     }
 
     public static void buildBatteryState(RecyclerAdapter.ViewHolder card) {
+        // Sample values
+        final String[] sampleLabels = {"Auto", "Haus"};
+        final float[][] sampleValues =
+                {{50f, 20f},  // % filled
+                {20f, 75f},  // % public used
+                {30f, 5f}}; // % not charged / empty
+
 
         card.textView.setText(R.string.card_battery_states);
 
@@ -66,15 +63,15 @@ public class CardFactory {
         card.chartLayout.addView(chart);
         chart.setLayoutParams(new LinearLayout.LayoutParams(card.chartLayout.getLayoutParams()));
 
-        BarSet stackBarSet = new BarSet(sampleLabels_batteryState, sampleValues_batteryState[0]);
+        BarSet stackBarSet = new BarSet(sampleLabels, sampleValues[0]);
         stackBarSet.setColor(Color.parseColor("#a1d949"));
         chart.addData(stackBarSet);
 
-        stackBarSet = new BarSet(sampleLabels_batteryState, sampleValues_batteryState[1]);
+        stackBarSet = new BarSet(sampleLabels, sampleValues[1]);
         stackBarSet.setColor(Color.parseColor("#ffcc6a"));
         chart.addData(stackBarSet);
 
-        stackBarSet = new BarSet(sampleLabels_batteryState, sampleValues_batteryState[2]);
+        stackBarSet = new BarSet(sampleLabels, sampleValues[2]);
         stackBarSet.setColor(Color.parseColor("#777777"));
         chart.addData(stackBarSet);
 
@@ -83,7 +80,7 @@ public class CardFactory {
 
         Paint thresPaint = new Paint();
         thresPaint.setColor(Color.parseColor("#dad8d6"));
-        thresPaint.setPathEffect(new DashPathEffect(new float[] {10, 20}, 0));
+        thresPaint.setPathEffect(new DashPathEffect(new float[]{10, 20}, 0));
         thresPaint.setStyle(Paint.Style.STROKE);
         thresPaint.setAntiAlias(true);
         thresPaint.setStrokeWidth(Tools.fromDpToPx(.75f));
